35efa3dfc52b41473c8b8f1c1f8154f02f2ba8e6
Genhao Li <1355401384@qq.com>
2022-08-03 18:31:28 +0800

    complete pix2code in pytorch

--- model/my_pix2code.py
+++ model/my_pix2code.py
@@ -2,9 +2,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torch
 
-from .Config import *
-from .AModel import *
-
 
 class ImageEncoder(nn.Module):
 
@@ -48,28 +45,50 @@ class ContextEncoder(nn.Module):
 
     def __init__(self):
         super(ContextEncoder, self).__init__()
+        self.rnn = nn.RNN(input_size=19, hidden_size=128, num_layers=2, batch_first=True)
 
-    def forward(self, x):
-        pass
-
+    def forward(self, x, h=None):
+        # x -> [-1, seq_size, 19], h -> [num_layer=2,-1, 128]
+        if not h:
+            h = torch.zeros((2, x.size(0), 128)).cuda()
+        x, _ = self.rnn(x, h)
+        return x
 
 class Decoder(nn.Module):
 
     def __init__(self):
         super(Decoder, self).__init__()
+        self.rnn = nn.RNN(input_size=1024 + 128, hidden_size=512, num_layers=2, batch_first=True)
+        self.l1 = nn.Linear(512, 19)
 
-    def forward(self, x):
-        pass
+    def forward(self, image_feature, context_feature, on_cuda=False, h=None):
+        # image_feature -> [-1, 1024], context_feature -> [-1, seq_size=48, 128], h -> [num_layer=2, -1, 512]
+        image_feature = image_feature.unsqueeze(1)
+        # image_feature -> [-1, 1, 1024]
+        image_feature = image_feature.repeat(1, context_feature.size(1), 1)
+        # image_feature -> [-1, seq_size, 1024]
+        x = torch.cat((image_feature, context_feature), 2)
+        # x -> [-1, seq_size=48, 1024+128]
+
+        if not h:
+            h = torch.zeros((2, x.size(0), 512)).cuda()
+
+        x, _ = self.rnn(x, h)
+        x = self.l1(x)
+        # x = F.softmax(x, dim=1)
+        return x
 
 
-class MyPix2code(nn.Module):
+class MyPix2Code(nn.Module):
 
     def __init__(self):
-        super(MyPix2code, self).__init__()
+        super(MyPix2Code, self).__init__()
         self.image_encoder = ImageEncoder()
         self.context_encoder = ContextEncoder()
         self.decoder = Decoder()
 
     def forward(self, image, context):
-        pass
-
+        image_feature = self.image_encoder(image)
+        context_feature = self.context_encoder(context)
+        output = self.decoder(image_feature, context_feature)
+        return output
